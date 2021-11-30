33a33cc6b188d311fad5286574492fd929b1b6ef
Khalimowski <93475605+Khalimowski@users.noreply.github.com>
2021-11-30 23:23:30 +0000

    Adding extra users

--- script.js
+++ script.js
@@ -2,7 +2,13 @@
 var database = [{
     username: "Kacper",
     password: "secret"
-}
+},
+{   username: "Jan",
+    password: "secret"},
+    {
+     username: "Stefan",
+     password: "tajny"   
+    }
 ]
 
 var newsFeed =[{
@@ -23,8 +29,8 @@ var userNamePrompt = prompt("What's your username?");
 var passwordPrompt = prompt("What's your password?");
 
 function signIn(user, pass) {
-    if (user === database[0].username && 
-        pass === database[0].password) {
+    if (user === database[0, 1, 2].username && 
+        pass === database[0, 1, 2].password) {
         console.log(newsFeed);
 } else {
     alert("Sorry, wrong username and password")
