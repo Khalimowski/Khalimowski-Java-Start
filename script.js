6547af05a1e5be40e4a4154eb5738a9ba73f52cb
Khalimowski <93475605+Khalimowski@users.noreply.github.com>
2021-12-01 23:48:07 +0000

    Loops

--- script.js
+++ script.js
@@ -1,40 +1,26 @@
+var todolist = [
+    "clean room",
+    "Brush Teeth",
+    "Press ups",
+    "Studya JavaScript",
+    "Eat three times a day",
+];
 
-var database = [{
-    username: "Kacper",
-    password: "secret"
-},
-{   username: "Jan",
-    password: "secret"},
-    {
-     username: "Stefan",
-     password: "tajny"   
-    }
-]
+var todolist2 = [
+    "clean rooms!",
+    "Brush Teeth1",
+    "Press ups!",
+    "Studya JavaScript!",
+    "Eat three times a day!",
+];
+var todolistLength = todolist.length
+//for (var i=0; i < todolistLength; i++) {
+ //   todolist.pop();
+//}
 
-var newsFeed =[{
-    username: "Tomek",
-    timeline: "Jestem Tata"
-
-},
-{
-    username: "Joanna",
-    timeline: "Jestem Mama"
-},
-{
-    username: "Krzysztof",
-    timeline: "Jestem Brat"
-}]
-
-var userNamePrompt = prompt("What's your username?");
-var passwordPrompt = prompt("What's your password?");
-
-function signIn(user, pass) {
-    if (user === database[0, 1, 2].username && 
-        pass === database[0, 1, 2].password) {
-        console.log(newsFeed);
-} else {
-    alert("Sorry, wrong username and password")
-    }
+function logsTodos(todo, i){
+    console.log(todo, i)
 }
 
-signIn(userNamePrompt, passwordPrompt);
+todolist.forEach (logsTodos)
+todolist2.forEach (logsTodos)
