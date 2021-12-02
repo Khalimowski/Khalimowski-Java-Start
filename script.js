8f6cb0a0bd5a6ab3361210327932589465fdb62c
Khalimowski <93475605+Khalimowski@users.noreply.github.com>
2021-12-02 22:49:21 +0000

    Loops 2

--- script.js
+++ script.js
@@ -1,26 +1,56 @@
 var todolist = [
-    "clean room",
-    "Brush Teeth",
-    "Press ups",
-    "Studya JavaScript",
-    "Eat three times a day",
-];
+    1 + " " + "Zdanie",
+    2 + " " + "Zdanie",
+    3 + " " + "Zdanie",
+    4 + " " + "Zdanie",
+    5 + " " + "Zdanie",
+    6 + " " + "Zdanie",
+    7 + " " + "Zdanie",
 
+];
 var todolist2 = [
     "clean rooms!",
-    "Brush Teeth1",
-    "Press ups!",
-    "Studya JavaScript!",
-    "Eat three times a day!",
-];
-var todolistLength = todolist.length
-//for (var i=0; i < todolistLength; i++) {
- //   todolist.pop();
+     "Brush Teeth!",
+     "Press ups!",
+     "Studya JavaScript!",
+     "Eat three times a day!",
+     "Ucz sie JavaScript",
+     "Dasz rade",
+         ];   
+
+for (var i=0; i < todolist.length; i++){
+    console.log(todolist[i] + " - " + todolist2[i])
+    
+          }
+
+ var WhileList = 0
+ 
+while (WhileList < 7) { 
+    console.log(WhileList + " " + todolist2[WhileList])
+    WhileList++
+}
+         
+         
+ 
+
+     
+     
+
+
+
+        
+    
+   
+    
+
+
+
+
+
+
+//function logsTodos(todo, i){
+//    console.log(todo, i)
 //}
 
-function logsTodos(todo, i){
-    console.log(todo, i)
-}
-
-todolist.forEach (logsTodos)
-todolist2.forEach (logsTodos)
+//todolist.forEach (logsTodos)
+//todolist2.forEach (logsTodos)
